a0b6ed479ee3373834bb3a29074001b58fbb2321
Dipper4ek <itworkermykola@gmail.com>
2025-05-22 19:08:00 +0200

    Python file added and server maked to work 3

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from typing import List
 
 app = FastAPI()
 
+
 class User(BaseModel):
     id: int
     username: str
     email: EmailStr
 
+
 users: List[User] = [
     User(id=1, username="john_doe", email="john@example.com"),
     User(id=2, username="alice", email="alice@example.com"),
-    User(id=3, username="bob", email="bob@example.com"),
 ]
 
-@app.get("/get_users", response_model=List[User])
+
+@app.get("/users", response_model=List[User])
 def get_users():
-    return {"message": "Good get did ", "List users: ": users}
+    return users
+
 
 @app.post("/post_users", response_model=List[User])
 def post_users(user: User):
     users.append(user)
-    return {"message": "Good post did ", "List users: ": users}
-
+    return users
